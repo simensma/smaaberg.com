eed5a7c169d13f17dc09c23c56b1699b9bdc12cb
Simen Fivelstad Smaaberg <simensma@gmail.com>
2020-05-07 14:54:32 -0700

    Load bg images as webp

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,16 +11,8 @@ import LoadingPage from '../components/LoadingPage';
 import $ from 'jquery';
 import MetaTags from 'react-meta-tags';
 
-const BGIMG = '/images/smaaberg-bg.jpg';
-
-const HP = styled.div`
-  background-image: linear-gradient(0deg, rgba(0,0,0,0.15), rgba(0,0,0,0.75)), url(${BGIMG});
-  background-attachment: fixed;
-  background-size: cover;
-  height: 100%;
-  width: 100%;
-  position: relative;
-`;
+const BGIMG = '/images/smaaberg-bg';
+const HIKEIMG = '/images/hiking-bg';
 
 
 
@@ -47,25 +39,49 @@ const DarkSection = styled(Section)`
   background-color: ${Theme().darkest};
 `;
 
-const ImageSection = styled(Section)`
-  background-image: linear-gradient(0deg, rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url('/images/hiking-bg.jpg');
+function withImgExt(webpSupport, path) {
+  return `${path}${webpSupport? '.webp': '.jpg'}`;
+}
+
+const ImageSectionCSS = (webpSupport) => styled(Section)`
+  background-image: linear-gradient(0deg, rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url(${withImgExt(webpSupport, HIKEIMG)});
   background-attachment: fixed;
   background-size: cover;
-
 `;
+
+const HPCSS = (webpSupport) => styled.div`
+  background-image: linear-gradient(0deg, rgba(0,0,0,0.15), rgba(0,0,0,0.75)), url(${withImgExt(webpSupport, BGIMG)});
+  background-attachment: fixed;
+  background-size: cover;
+  height: 100%;
+  width: 100%;
+  position: relative;
+`;
+
+
 
 class Page extends React.Component {
 
   constructor() {
     super();
 
-    this.state = {loaded: false};
+    this.state = {loaded: false, webpSupport: this.canUseWebp()};
     this.content = this.content.bind(this);
   }
  
+  canUseWebp() {
+    const elem = document.createElement('canvas');
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+      // was able or not to get WebP representation
+      return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    } else {
+      return false;
+    }
+  }
+
   componentDidMount() {
     const self = this;
-    $('<img/>').attr('src', BGIMG).on('load', function() {
+    $('<img/>').attr('src', withImgExt(this.state.webpSupport, BGIMG)).on('load', function() {
       $(this).remove();
 
       self.setState({loaded: true});
@@ -73,6 +89,9 @@ class Page extends React.Component {
   }
 
   content() {
+    const HP = HPCSS(this.state.webpSupport);
+    const ImageSection = ImageSectionCSS(this.state.webpSupport);
+
     if(this.state.loaded) {
       return (
         <MainPage>
